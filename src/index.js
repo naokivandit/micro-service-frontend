a21a51a95be2afdce8afe534679db501146b1f8a
naokivandit <n.i.vandit0924@gmail.com>
2022-02-23 18:17:29 +0900

    fix src/index

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
 import React from "react";
 import { render } from "react-dom";
-import {
-  ApolloProvider,
-} from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
 
 import { client } from "./lib/apollo/client";
 import { api } from "./services/api";
 
-const ExchangeRates = () => {
+const Pokemons = () => {
   const { data } = api.pokemon.usePokemon({
-    first:10
-  })
-  console.log(data)
-  return ""
-}
+    first: 10,
+  });
+  console.log(data);
+  return <div></div>;
+};
 
 function App() {
   return (
     <div>
       <h2>My first Apollo app</h2>
-      <ExchangeRates />
     </div>
   );
 }
@@ -27,6 +24,7 @@ function App() {
 render(
   <ApolloProvider client={client}>
     <App />
+    <Pokemons />
   </ApolloProvider>,
   document.getElementById("root")
 );
